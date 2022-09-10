d531462aa8739dc45a09a2184a81cea9c9d4eb15
hoonkim <hoonk9387@gmail.com>
2022-09-10 13:27:13 -0500

    Recipes routes index and create.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # resources :recipes, only: [:index, :create]
-
+  
+  # Recipes view and create
+  resources :recipes, only: [:index, :create]
   # Sign-up feature
   post "/signup", to: "users#create"
   # Auto-Login feature
